cef2b4371b2ff9caeea3d344d342b6d2e7cccf77
Dave Bailey <dave.andrew.bailey@gmail.com>
2022-05-03 11:29:10 -0400

    editing files for heroku practice

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
 
 app.use(cors());
 
-//handles a content-type header for the server to use if it is json
+
 app.use(express.json());
 
-//allows us to understand urlenconded payload
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -31,16 +30,6 @@ app.get("/", async (request, response) => {
 
   return response.json(restaurants);
 })
-
-// // Accessing the path module
-// const path = require("path");
-
-// // Step 1:
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
-// // Step 2:
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
  
 app.listen(port, () => {
   console.log(`You're listening on ${port}`);
