949fea4c68aaaa1026966fe5c21a87ab4d8f56cc
Dave Bailey <dave.andrew.bailey@gmail.com>
2022-05-03 11:11:05 -0400

    heroku test

--- server.js
+++ server.js
@@ -17,9 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const restaurantSchema = require("./data.js");
+const res = require("express/lib/response");
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
+app.get("/", (rec, res) => {
+  res.send("Hello World")
+})
 
 app.get("/", async (request, response) => {
 
