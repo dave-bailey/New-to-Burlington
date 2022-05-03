a242a66345b10643155aedf40c2979a6feb2a1e4
Dave Bailey <dave.andrew.bailey@gmail.com>
2022-05-03 12:58:36 -0400

    heroku test

--- server.js
+++ server.js
@@ -27,17 +27,6 @@ app.get("/", async (request, response) => {
 
   return response.json(restaurants);
 })
- 
-
-// Accessing the path module
-const path = require("path");
-
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 app.listen(port, () => {
   console.log(`You're listening on ${port}`);
