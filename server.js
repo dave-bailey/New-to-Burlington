8a854bfed98ce0aa1d4dd81c868ad4251b5812ec
Dave Bailey <dave.andrew.bailey@gmail.com>
2022-05-03 09:24:21 -0400

    adding build and client dependencies

--- server.js
+++ server.js
@@ -31,6 +31,16 @@ let restaurantData = restaurants.filter((restaurants) => {
   response.json(restaurantData);
 });
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`You're listening on ${port}`);
 });
